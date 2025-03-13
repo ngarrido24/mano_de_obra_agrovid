cf42ba176b2540a406d6b8056db0c95bf850fcaa
Natalia Garrido <garridogna@gmail.com>
2025-03-13 18:24:21 -0500

    configuraciones iniciales para despliegue

--- functions_mo.py
+++ functions_mo.py
@@ -1,14 +1,19 @@
 import logging
 import pandas as pd
 from datetime import datetime, timedelta
-
+from logging import Logger
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError
+from mypy_boto3_dynamodb.client import DynamoDBClient
+from mypy_boto3_s3.client import S3Client
+from chalice import Chalice
+from chalice import Response
+from io import StringIO
 # Set up logging
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
-
 
 
 def final_result(dataset, year, number):
@@ -84,3 +89,174 @@ def calculate_volume_distribution(volum_file_emb_subset_def, volum_distribution_
     except Exception as e:
         logger.error(f"Error durante el cálculo de la distribución de volumen: {e}")
         raise
+
+"-------------------------------------------------------------------------------------------------"
+
+def object_to_dataframe(
+    s3_client: S3Client,
+    bucket_name: str,
+    folder_name: str,
+    partition: str,
+    file_name: str,
+    encoded: str = "utf-8",
+    sep: str = ";",
+) -> pd.DataFrame:
+    """
+    The function `object_to_dataframe` reads a CSV file from an S3 bucket using an S3 client, decodes
+    the data, and returns it as a Pandas DataFrame, with an option to specify the separator.
+
+    :param s3_client: S3Client object from the boto3 library, used for interacting with Amazon S3
+    :type s3_client: S3Client
+    :param bucket_name: The `bucket_name` parameter in the `object_to_dataframe` function refers to the
+    name of the Amazon S3 bucket where the object is stored. This is the bucket from which the function
+    will retrieve the object specified by the `key` parameter
+    :type bucket_name: str
+    :param key: The `key` parameter in the `object_to_dataframe` function is a string that represents
+    the key of the object stored in an S3 bucket. It is used to specify the specific object that you
+    want to retrieve and convert into a pandas DataFrame
+    :type key: str
+    :param encoded: The `encoded` parameter in the `object_to_dataframe` function specifies the encoding
+    format used to decode the data read from the S3 object before converting it into a DataFrame. In
+    this case, the default encoding format is set to "utf-8", but you can specify a different encoding
+    format if, defaults to utf-8
+    :type encoded: str (optional)
+    :param sep: The `sep` parameter in the `object_to_dataframe` function is used to specify the
+    delimiter that separates columns in the CSV file being read. By default, the delimiter is set to
+    `;`. However, if the `key` parameter matches either "ER_SIMULADO/EXCEDENTE.csv, defaults to ;
+    :type sep: str (optional)
+    :return: A pandas DataFrame containing the data from the specified object in the S3 bucket.
+    """
+
+    # if file_name in (
+    #     "EXCEDENTE.csv",
+    #     "platano.csv",
+    # ):
+    #     sep = ","
+
+    # if file_name == "EXCEDENTE.csv":
+    #     encoded = "latin-1"
+
+    key = f"{folder_name}/{partition}/{file_name}"
+
+    obj = s3_client.get_object(Bucket=bucket_name, Key=key)
+    data = obj["Body"].read().decode(encoded)
+    df = pd.read_csv(StringIO(data), sep=sep)
+    return df
+
+
+"-------------------------------------------------------------------------------------------------"
+
+def get_item_from_dynamodb_global(table_name: str, partition_key_value: str, logger: Logger, dynamodb_client):
+    """
+    Retrieves an item from a DynamoDB table without requiring a sort key if not needed.
+
+    :param table_name: Name of the DynamoDB table.
+    :param partition_key_value: Value of the partition key.
+    :param logger: Logger instance for logging.
+    :param dynamodb_client: Boto3 DynamoDB resource.
+    :return: The retrieved item or None if not found.
+    """
+    try:
+        # Access the table
+        table = dynamodb_client.Table(table_name)
+
+        # Get table key schema
+        table_info = dynamodb_client.meta.client.describe_table(TableName=table_name)
+        key_schema = table_info["Table"]["KeySchema"]
+
+        # Identify partition key and sort key (if exists)
+        key_dict = {key["AttributeName"]: partition_key_value for key in key_schema if key["KeyType"] == "HASH"}
+
+        # If there is a sort key, add it (this assumes a default or required value)
+        sort_keys = [key["AttributeName"] for key in key_schema if key["KeyType"] == "RANGE"]
+        if sort_keys:
+            logger.warning(f"Table {table_name} requires a sort key: {sort_keys}. Consider adjusting your query.")
+
+        # Get the item
+        response = table.get_item(Key=key_dict)
+
+        # Check if the item exists in the response
+        if "Item" in response:
+            return response["Item"]
+        else:
+            logger.error(f"Item not found in table {table_name}.")
+            return None
+
+    except NoCredentialsError:
+        logger.exception("AWS credentials not found.")
+        raise Exception("AWS credentials not found.")
+    except PartialCredentialsError:
+        logger.exception("Incomplete AWS credentials configuration.")
+        raise Exception("Incomplete AWS credentials configuration.")
+    except Exception as e:
+        logger.exception(f"An error occurred: {e}")
+        raise Exception(f"An error occurred: {e}")
+
+"-------------------------------------------------------------------------------------------------"
+
+
+def get_item_from_dynamodb(table_name: str, key: dict, logger: Logger, dynamodb_client):
+    """
+    Retrieves an item from a DynamoDB table.
+
+    :param table_name: Name of the DynamoDB table.
+    :param key: Dictionary representing the key of the item to retrieve.
+                 Example: {'partition_key': 'value', 'sort_key': 'value'}
+    :return: The retrieved item or None if not found.
+    """
+    try:
+        # Access the table
+        table = dynamodb_client.Table(table_name)
+
+        # Get the item
+        response = table.get_item(Key=key)
+
+        # Check if the item exists in the response
+        if "Item" in response:
+            return response["Item"]
+        else:
+            logger.error(f"Item not found in table {table_name}.")
+            Exception(f"Item not found in table {table_name}.")
+
+    except NoCredentialsError:
+        logger.error("AWS credentials not found.")
+        Exception("AWS credentials not found.")
+    except PartialCredentialsError:
+        logger.error("Incomplete AWS credentials configuration.")
+        Exception("Incomplete AWS credentials configuration.")
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        Exception(f"An error occurred: {e}")  
+
+
+"-------------------------------------------------------------------------------------------------"
+
+def get_ssm_parameter(parameter_name, logger: Logger, ssm_client, with_decryption=True):
+    """
+    Retrieve a parameter from AWS SSM Parameter Store.
+
+    :param parameter_name: The name of the SSM parameter to retrieve.
+    :param with_decryption: Boolean indicating if the parameter should be decrypted (for SecureString parameters).
+    :param region_name: AWS region where the SSM parameter store is located.
+    :return: The value of the SSM parameter.
+    """
+    try:
+        # Retrieve the parameter
+        response = ssm_client.get_parameter(
+            Name=parameter_name, WithDecryption=with_decryption
+        )
+        logger.info(f"Parameter {parameter_name} retrieved successfully.")
+        return response["Parameter"]["Value"]
+
+    except NoCredentialsError:
+        logger.error("AWS credentials not found.")
+        Exception("AWS credentials not found.")
+    except PartialCredentialsError:
+        logger.error("Incomplete AWS credentials configuration.")
+        Exception("Incomplete AWS credentials configuration.")
+    except ssm_client.exceptions.ParameterNotFound:
+        logger.error(f"Parameter {parameter_name} not found.")
+        Exception(f"Parameter {parameter_name} not found.")
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        Exception(f"An error occurred: {str(e)}")
